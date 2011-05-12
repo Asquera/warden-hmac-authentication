46c5f82de2f3f8b4c4d40bf9ef3ddd6b6dde726d
Felix Gilcher <felix.gilcher@asquera.de>
2011-05-12 13:46:47 +0200

    add tests for empty secrets

--- test/hmac_strategy_test.rb
+++ test/hmac_strategy_test.rb
@@ -58,6 +58,46 @@ context "HMAC" do
     
   end
   
+  context "> with an empty secret " do
+    app(
+      Rack::Builder.new do
+        use Rack::Session::Cookie
+        use Warden::Manager do |manager|
+          manager.failure_app = -> env { [401, {"Content-Length" => "0"}, [""]] }
+          manager.default_scope = :default
+          manager.scope_defaults :default, :strategies => [:password, :basic]
+          manager.scope_defaults :token, :strategies => [:hmac], 
+                                         :store => false, 
+                                         :hmac => { 
+                                           :params => ["user_id"],
+                                           :token => "token",
+                                           :secret => "",
+                                           :algorithm => "md5",
+                                           :hmac => HMAC
+                                         }
+        end
+      
+        run -> env {
+          env["warden"].authenticate!(:scope => :token)
+          [200, {"Content-Length" => "0"}, [""]]
+        }
+      end.to_app
+    ) 
+
+    context "> with a valid signature" do
+      setup do
+        uri = "http://example.org/?user_id=123"
+        signed = uri + "&token=" + HMAC.new('md5').generate_signature(uri, '')
+      
+        get signed
+      end
+      
+      asserts(:status).equals(401)
+      
+    end
+  end
+  
+  
   context "> with a proc as secret " do
     app(
       Rack::Builder.new do
