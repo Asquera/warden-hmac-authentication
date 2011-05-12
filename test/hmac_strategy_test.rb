953ea6d98dc0532435bc6f3c6b885d4ffbb70394
Felix Gilcher <felix.gilcher@asquera.de>
2011-05-12 12:18:41 +0200

    allow procs as secrets change Hmac to accept the secret when signing the url

--- test/hmac_strategy_test.rb
+++ test/hmac_strategy_test.rb
@@ -56,8 +56,64 @@ context "HMAC" do
       asserts(:status).equals(401)
     end
     
-    
   end
+  
+  context "> with a proc as secret " do
+    app(
+      Rack::Builder.new do
+        use Rack::Session::Cookie
+        use Warden::Manager do |manager|
+          manager.failure_app = -> env { [401, {"Content-Length" => "0"}, [""]] }
+          manager.default_scope = :default
+          manager.scope_defaults :default, :strategies => [:password, :basic]
+          manager.scope_defaults :token, :strategies => [:hmac], 
+                                         :store => false, 
+                                         :hmac => { 
+                                           :params => ["user_id"],
+                                           :token => "token",
+                                           :secret => Proc.new {|strategy|
+                                             "secrit"
+                                           },
+                                           :algorithm => "md5",
+                                           :hmac => HMAC
+                                         }
+        end
+      
+        run -> env {
+          env["warden"].authenticate!(:scope => :token)
+          [200, {"Content-Length" => "0"}, [""]]
+        }
+      end.to_app
+    )
+    
+    context "> with a valid signature" do
+      setup do
+        uri = "http://example.org/?user_id=123"
+        signed = uri + "&token=" + HMAC.new('md5').generate_signature(uri, 'secrit')
+      
+        get signed
+      end
+
+      asserts(:status).equals(200)
+    end
+    
+    context "> with no signature" do
+      setup do
+        get "http://example.org/?user_id=123&token=foo"
+      end
+
+      asserts(:status).equals(401)
+    end
+    
+    context "> with an invalid signature" do
+      setup do
+        get "http://example.org/?user_id=123&token=foo"
+      end
+
+      asserts(:status).equals(401)
+    end
+  end
+    
 end
 
 
