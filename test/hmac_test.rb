26b32ca395d1f064575a8ccbc3881984dd0a4df5
Felix Gilcher <felix.gilcher@asquera.de>
2011-05-12 16:49:19 +0200

    added a method to sign a url

--- test/hmac_test.rb
+++ test/hmac_test.rb
@@ -18,7 +18,7 @@ context "an HMAC object" do
     
   end
 
-  context "> signing a url" do
+  context "> generating the signature of a url" do
     
     setup do
       topic.generate_signature("http://example.org?foo=bar&baz=foobar", "secret")
@@ -29,8 +29,9 @@ context "an HMAC object" do
     asserts("token parameter is not taken into consideration") { topic == HMAC.new("md5").generate_signature("http://example.org?foo=bar&baz=foobar&token=4e8488fcfdea30d400861973ba03e079", "secret") }
   end
 
-  asserts("checking a url_signature") {   topic.check_signature("http://example.org?foo=bar&baz=foobar&token=4e8488fcfdea30d400861973ba03e079", "secret") }
-  denies("checking an invalid url_signature") {   topic.check_signature("http://example.org?foo=bar&baz=foobar&token=888488fcfdea30d400861973ba03e079", "secret") }
+  asserts("checking a url_signature") { topic.check_signature("http://example.org?foo=bar&baz=foobar&token=4e8488fcfdea30d400861973ba03e079", "secret") }
+  denies("checking an invalid url_signature") { topic.check_signature("http://example.org?foo=bar&baz=foobar&token=888488fcfdea30d400861973ba03e079", "secret") }
   
+  asserts("signing a url") { topic.sign_url("http://example.org?foo=bar&token=888488fcfdea30d400861973ba03e079", "secret", "token", {"baz" => "foobar"}) }.equals("http://example.org?baz=foobar&foo=bar&token=4e8488fcfdea30d400861973ba03e079")
   
 end
