e4d3ae0081337beff752297b62fcb8739ee348a2
Florian Gilcher <florian.gilcher@asquera.de>
2012-04-13 17:01:47 +0200

    Catch the case where no query is given

--- lib/hmac/signer.rb
+++ lib/hmac/signer.rb
@@ -123,8 +123,9 @@ module HMAC
 
       uri = Addressable::URI.parse(url)
       query_values = uri.query_values
-      auth_params = query_values.delete(opts[:auth_param])
+      return false unless query_values
 
+      auth_params = query_values.delete(opts[:auth_param])
       return false unless auth_params
 
       date = auth_params["date"]
