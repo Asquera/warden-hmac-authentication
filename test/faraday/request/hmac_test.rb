04cd9d82d6e397f569e49137f5ff05d741b0fe45
Jon Rowe <hello@jonrowe.co.uk>
2013-03-06 10:06:45 +1100

    ws

--- test/faraday/request/hmac_test.rb
+++ test/faraday/request/hmac_test.rb
@@ -15,36 +15,36 @@ end
 
 
 context "the faraday middleware" do
-  
+
   setup do
     Timecop.freeze Time.gm(2011, 7, 1, 20, 28, 55)
   end
-  
+
   teardown do
     Timecop.return
   end
-  
+
   context "> using header-based auth" do
     setup do
       m = Faraday::Request::Hmac.new(DummyApp.new, "testsecret")
       m.call({ :request_headers => {}, :url => 'http://www.example.com' })
     end
-  
+
     asserts("authorization header") {topic[:request_headers]["Authorization"]}.equals("HMAC 539263f4f83878a4917d2f9c1521320c28b926a9")
     asserts("date header") {topic[:request_headers]["Date"]}.equals("Fri,  1 Jul 2011 20:28:55 GMT")
     asserts("query values") {topic[:url].query}.nil
-    
+
     context "> using a different auth header format" do
       setup do
         m = Faraday::Request::Hmac.new(DummyApp.new, "testsecret", {:auth_key => 'TESTKEYID', :auth_header_format => '%{auth_scheme} %{auth_key} %{signature}'})
         m.call({ :request_headers => {}, :url => 'http://www.example.com' })
       end
-  
+
       asserts("authorization header") {topic[:request_headers]["Authorization"]}.equals("HMAC TESTKEYID 539263f4f83878a4917d2f9c1521320c28b926a9")
       asserts("date header") {topic[:request_headers]["Date"]}.equals("Fri,  1 Jul 2011 20:28:55 GMT")
       asserts("query values") {topic[:url].query}.nil
     end
-    
+
   end
 
   context "> using query-based auth" do
@@ -52,21 +52,21 @@ context "the faraday middleware" do
       m = Faraday::Request::Hmac.new(DummyApp.new, "testsecret", {:query_based => true, :extra_auth_params => {"auth_key" => "TESTKEYID"}})
       m.call({ :request_headers => {}, :url => 'http://www.example.com' })
     end
-  
+
     asserts("authorization header") {topic[:request_headers]["Authorization"]}.nil
     asserts("date header") {topic[:request_headers]["Date"]}.nil
-    
+
     context "> query values" do
-      
+
       setup do
         Rack::Utils.parse_nested_query(topic[:url].query)
       end
-    
+
       asserts("auth date") {topic["auth"]["date"]}.equals("Fri,  1 Jul 2011 20:28:55 GMT")
       asserts("auth_key") {topic["auth"]["auth_key"]}.equals("TESTKEYID")
       asserts("auth signature") {topic["auth"]["signature"]}.equals("539263f4f83878a4917d2f9c1521320c28b926a9")
     end
-    
+
   end
 
 
--- test/hmac/signer_test.rb
+++ test/hmac/signer_test.rb
@@ -6,7 +6,7 @@ context "an HMAC object" do
   end
 
   context "> generating the canonical representation" do
-    
+
     asserts("representation with nonce"){
       topic.canonical_representation({
         :method => "GET",
@@ -19,7 +19,7 @@ context "an HMAC object" do
         }
       })
     }.equals("GET\ndate:Mon, 20 Jun 2011 12:06:11 GMT\nnonce:TESTNONCE\n/example?baz=foobared&foo=bar")
-    
+
     asserts("representation with headers"){
       topic.canonical_representation({
         :method => "GET",
@@ -35,34 +35,34 @@ context "an HMAC object" do
           "Content-MD5" => "d41d8cd98f00b204e9800998ecf8427e"
         }
       })
-    }.equals("GET\ndate:Mon, 20 Jun 2011 12:06:11 GMT\nnonce:TESTNONCE\ncontent-md5:d41d8cd98f00b204e9800998ecf8427e\ncontent-type:application/json;charset=utf8\n/example?baz=foobared&foo=bar")    
-    
+    }.equals("GET\ndate:Mon, 20 Jun 2011 12:06:11 GMT\nnonce:TESTNONCE\ncontent-md5:d41d8cd98f00b204e9800998ecf8427e\ncontent-type:application/json;charset=utf8\n/example?baz=foobared&foo=bar")
+
   end
 
   context "> generating the signature for a request" do
-    
+
     setup do
       topic.sign_request("http://example.org?foo=bar&baz=foobar", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :nonce => "TESTNONCE")
     end
 
     context "> resulting headers" do
-      
+
       setup do
         topic[0]
       end
-      
+
       asserts("date header") {topic["Date"]}.equals("Mon, 20 Jun 2011 12:06:11 GMT")
       asserts("nonce header") {topic["X-HMAC-Nonce"]}.equals("TESTNONCE")
       asserts("authorization header") {topic["Authorization"]}.equals("HMAC b2c5c7242f664ce18828f108452b437b")
 
     end
-    
+
     asserts("resulting url is") {topic[1]}.equals("http://example.org?foo=bar&baz=foobar")
     asserts("query parameter order does not matter") do
       headers, url = * HMAC::Signer.new("md5").sign_request("http://example.org?baz=foobar&foo=bar", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :nonce => "TESTNONCE")
       headers["Authorization"] == topic[0]["Authorization"]
     end
-    
+
   end
 
   context "> generating the signature for a POST request" do
@@ -82,10 +82,10 @@ context "an HMAC object" do
     end
 
   end
-  
+
   context "> generating the signature while ignoring some params" do
     helper(:signer) { HMAC::Signer.new('md5') }
-    
+
     setup do
       signer.sign_request("http://example.org?foo=bar&baz=foobar", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :ignore_params => [:baz])
     end
@@ -100,7 +100,7 @@ context "an HMAC object" do
     setup do
       topic.sign_url("http://example.org?foo=bar&baz=foobar", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :nonce => "TESTNONCE")
     end
-    
+
     asserts("base url") {topic.split("?")[0]}.equals("http://example.org")
     asserts("parameters") {  Rack::Utils.parse_query(topic.split("?")[1]) }.equals({
       "foo"         => "bar",
@@ -109,28 +109,28 @@ context "an HMAC object" do
       "auth[signature]"=>"b2c5c7242f664ce18828f108452b437b",
       "auth[nonce]"=>"TESTNONCE"
     })
-    
+
   end
 
   context "signing a url without query parameters" do
     setup do
       topic.sign_url("http://example.org/example.html", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :nonce => "TESTNONCE")
     end
-    
+
     asserts("base url") {topic.split("?")[0]}.equals("http://example.org/example.html")
     asserts("parameters") {  Rack::Utils.parse_query(topic.split("?")[1]) }.equals({
       "auth[date]"  =>"Mon, 20 Jun 2011 12:06:11 GMT",
       "auth[signature]"=>"b0287a82bc0d36aef01dd8094c2e2814",
       "auth[nonce]"=>"TESTNONCE"
     })
-    
+
   end
 
   context "signing a url with extra auth parameters" do
     setup do
       topic.sign_url("http://example.org/example.html", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT", :nonce => "TESTNONCE", :extra_auth_params => {:access_key_id => "KEY1"})
     end
-    
+
     asserts("base url") {topic.split("?")[0]}.equals("http://example.org/example.html")
     asserts("parameters") {  Rack::Utils.parse_query(topic.split("?")[1]) }.equals({
       "auth[date]"  =>"Mon, 20 Jun 2011 12:06:11 GMT",
@@ -138,14 +138,14 @@ context "an HMAC object" do
       "auth[nonce]"=>"TESTNONCE",
       "auth[access_key_id]" => "KEY1"
     })
-    
+
   end
 
   context "signing a url with a fragment" do
     setup do
       topic.sign_url("http://www.example.org/foo?example=bar&bar=baz#somewhere", "secret", :date => "Mon, 20 Jun 2011 12:06:11 GMT")
     end
-    
+
     asserts("base url") {topic.split("?")[0]}.equals("http://www.example.org/foo")
     asserts("parameters") {  Rack::Utils.parse_query(topic.split("?")[1].split("#")[0]) }.equals({
       "example" => "bar",
@@ -154,12 +154,12 @@ context "an HMAC object" do
       "auth[signature]"=>"4b6c1ea41fbb1c83010ebabbbd6f98e6"
     })
     asserts("fragment") {topic.split("#")[1]}.equals("somewhere")
-  
+
   end
 
   asserts("checking a url_signature") { topic.validate_url_signature("http://example.org?baz=foobar&foo=bar&auth[date]=Mon%2C%2020%20Jun%202011%2012%3A06%3A11%20GMT&auth[signature]=b2c5c7242f664ce18828f108452b437b&auth[nonce]=TESTNONCE", "secret") }
   denies("checking an invalid url_signature") { topic.validate_url_signature("http://example.org?baz=foobar&foo=bar&auth[date]=Mon%2C%2020%20Jun%202011%2012%3A06%3A11%20GMT&auth[signature]=AAc5c7242f664ce18828f108452b437b&auth[nonce]=TESTNONCE", "secret") }
-  
+
   denies("checking a url without auth parameters in query string") { topic.validate_url_signature("http://example.org?foo=bar", "secret") }
   denies("checking a url without query string") { topic.validate_url_signature("http://example.org", "secret") }
 end
